c18c4063da60c4fdfa2408fe44701d73827dd69d
Saksham <cedaus97@gmail.com>
2019-11-22 18:42:31 +0530

    minor

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
-import thunk from 'redux-thunk';
-import {createStore, applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {createLogger} from 'redux-logger'
+import {applyMiddleware, createStore} from 'redux';
 import RootReducer from "./src/main/RootReducer";
 import RootNavigator from "./src/main/RootNavigator";
+import fetchUserPosts from 'feeds/FeedService';
+import {fetchPostsPending} from "feeds/FeedActions";
 
+const loggerMiddleware = createLogger();
 
-const middlewares = [thunk];
-
-const store = createStore(RootReducer, applyMiddleware(...middlewares));
+const store = createStore(RootReducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
+store.dispatch(fetchPostsPending());
+store.dispatch(fetchUserPosts()).then(() => console.log(store.getState()));
 
 export default class App extends Component {
   render() {
